3d596f49bb509fff64bf8345e2ca96c88e61314e
aziz_78 <aziz.fre9@gmail.com>
2025-09-25 11:17:16 +0500

    started work on catalog service

---
+++ src/services/catalog.service.ts
@@ -0,0 +1,31 @@
+import { ICatalogRepository } from "../interface/catalogRepository.interface";
+
+export class CatalogService{
+    private _repository: ICatalogRepository;
+    constructor(repository:ICatalogRepository){
+        this._repository = repository
+    }
+    async createProduct(input:any){
+        const data = await this._repository.create(input)
+        if(!data.id){
+            throw new Error("unable to create product")
+        }
+        return data
+    };
+    async updateProduct(input:any){
+        const data = await this._repository.update(input)
+        return data
+    };
+    async getProducts(limit:number,offset:number){
+        const data = await this._repository.find(limit,offset)
+        return data
+    };
+    async getProduct(id:number){
+        const data = await this._repository.findOne(id)
+        return data
+    };
+    async deleteProduct(id:number){
+        const response = await this._repository.delete(id)
+        return response
+    };
+}
